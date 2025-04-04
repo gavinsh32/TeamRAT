bf4420b2bb81b0a9d19d748c852e44b6c6388a4b
gavinsh32 <gavinhaynes32@gmail.com>
2025-04-04 16:42:00 -0700

    Working RLE to mask converter

--- compare.py
+++ compare.py
@@ -1,6 +1,7 @@
 # compare.py
 # Gavin Haynes
 # View input images along with annotations.
+# THIS CODE IS NOT MINE: https://stackoverflow.com/a/74901690
 
 import os
 import cv2 as cv
@@ -8,16 +9,78 @@ import numpy
 import json
 from itertools import groupby
 from pycocotools import mask as mask_utils
+from typing import List
+import numpy as np
 
-mask = cv.imread('./dataset/masks/image-1-mask-0.png', cv.COLOR_RGB2GRAY)
 
-def binary_mask_to_rle(binary_mask):
-    rle = {'counts': [], 'size': list(binary_mask.shape)}
-    counts = rle.get('counts')
-    for i, (value, elements) in enumerate(groupby(binary_mask.ravel(order='F'))):
-        if i == 0 and value == 1:
-            counts.append(0)
-        counts.append(len(list(elements)))
-    return rle
+class InputStream:
+    def __init__(self, data):
+        self.data = data
+        self.i = 0
 
-print(binary_mask_to_rle(mask))
+    def read(self, size):
+        out = self.data[self.i:self.i + size]
+        self.i += size
+        return int(out, 2)
+
+
+def access_bit(data, num):
+    """ from bytes array to bits by num position"""
+    base = int(num // 8)
+    shift = 7 - int(num % 8)
+    return (data[base] & (1 << shift)) >> shift
+
+
+def bytes2bit(data):
+    """ get bit string from bytes data"""
+    return ''.join([str(access_bit(data, i)) for i in range(len(data) * 8)])
+
+
+def rle_to_mask(rle: List[int], height: int, width: int) -> np.array:
+    """
+    Converts rle to image mask
+    Args:
+        rle: your long rle
+        height: original_height
+        width: original_width
+
+    Returns: np.array
+    """
+
+    rle_input = InputStream(bytes2bit(rle))
+
+    num = rle_input.read(32)
+    word_size = rle_input.read(5) + 1
+    rle_sizes = [rle_input.read(4) + 1 for _ in range(4)]
+    # print('RLE params:', num, 'values,', word_size, 'word_size,', rle_sizes, 'rle_sizes')
+
+    i = 0
+    out = np.zeros(num, dtype=np.uint8)
+    while i < num:
+        x = rle_input.read(1)
+        j = i + 1 + rle_input.read(rle_sizes[rle_input.read(2)])
+        if x:
+            val = rle_input.read(word_size)
+            out[i:j] = val
+            i = j
+        else:
+            while i < j:
+                val = rle_input.read(word_size)
+                out[i] = val
+                i += 1
+
+    image = np.reshape(out, [height, width, 4])[:, :, 3]
+    return image
+
+with open('./data.json') as data_file:
+    data = json.load(data_file)
+
+    for task in data:
+        for annotation in task['annotations'][0]['result']:
+            rle = annotation['value']['rle']
+            width = annotation['original_width']
+            height = annotation['original_height']
+            print(rle, width, height)
+            mask = rle_to_mask(rle, height, width)
+            cv.imshow('Result', mask)
+            cv.waitKey(0)
