9ddd470f1c0d39c15757ce6efc68dbde4ec63feb
gavinsh32 <gavinhaynes32@gmail.com>
2025-04-05 14:30:54 -0700

    Adding updated json and some more images

--- dataset.py
+++ dataset.py
@@ -6,14 +6,32 @@ import json
 import labelconverter as lc
 import numpy as np
 import cv2 as cv
+import os
 from pathlib import Path
 
+
 class Dataset:
-    def __init__(self, images_path: Path, annotations_path: Path):
-        self.images_path = images_path.resolve()
-        self.annotations_path = annotations_path.resolve()
-        print(self.images_path)
-        print(self.annotations_path)
+
+    def __init__(self, dataset_folder: Path):
+
+        dataset_folder = dataset_folder.resolve()
+        imgs_folder = dataset_folder / 'imgs'
+
+        # Read in all image paths
+        self.img_paths = [path for path in imgs_folder.glob('*')]
+
+        # Check that there are images in the image folder
+        if len(self.img_paths) < 1:
+            print('ERROR: found no images in', imgs_folder)
+
+        # Seek values for iterating through the JSON file
+        self.taskNum = 0
+        self.labelNum = 0
+
+        # Read in essential data from labels
+        labels_file = dataset_folder / 'labels.json'
+        with open(labels_file) as data_file:
+            self.data = json.load(data_file)
 
     def check(self, imgs_path: Path):
         pass
@@ -22,10 +40,8 @@ class Dataset:
     def loadJSON(self, json_path: Path) -> bool:
         return True
 
-cwd = Path.cwd().absolute()
-imgs_path = cwd.joinpath('imgs')
+dataset_folder = Path('./dataset').resolve()
 
-print(cwd)
-print(imgs_path)
+print(dataset_folder)
 
-# dataset = Dataset('./imgs/')
+dataset = Dataset(dataset_folder)
