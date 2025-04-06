0fb8e6c4028a08538b6e34b2425592748021dc3b
gavinsh32 <gavinhaynes32@gmail.com>
2025-04-06 12:07:50 -0700

    - updated dict to store str instead of path for image path - removed old labels - added sum comments

--- dataset.py
+++ dataset.py
@@ -2,65 +2,76 @@
 # Gavin Haynes
 # Read in Label Studio Annotations, fetch labels, and check your dataset.
 
-import json
-import labelconverter as lc
-import numpy as np
-import cv2 as cv
-import os
-from pathlib import Path
+import json                     # Reading annotations in
+import numpy as np              # Mask and image data
+import cv2 as cv                # Display results
+import os                       # System operations
+from pathlib import Path        # Path manipulation
+import labelconverter as lc     # See labelconvert.py, I did not write this
 
 class Dataset:
 
     # Read in dataset and load valid images and annotations
     def __init__(self, dataset_folder_path: str):
+        """
+        Reads in a dataset and stores a cleaned internal copy.
+        Args:
+            dataset_folder_path: name of dataset folder
+        Returns: None
+        """
 
         dataset_folder = Path(dataset_folder_path).resolve()
         imgs_folder = dataset_folder / 'imgs'
-
-        # Read in essential data from labels
         labels_file = dataset_folder / 'labels.json'
+        
+        self.data = {}      # Keys: image path, Content: dict with img data
+        data = None         # Original JSON file     
+        
+        # Open JSON file and read in
         with open(labels_file) as data_file:
-
-            self.data = {}
-
             data = json.load(data_file)
 
-            # Grab image path from json
-            for task in data:
-                # Grab name of matching file, trim, and append to folder path
-                img_path = imgs_folder / task['file_upload'].split('-')[1]
+        # Iterate through each task (set of annotations, 1 per input image)
+        for task in data:
+            
+            # Grab name of the source image that was annotated, and trim
+            img_name = task['file_upload'].split('-')[1]
+            img_path = str(imgs_folder / img_name)
 
-                if os.path.exists(img_path):
+            # Only read in annotations if there is a matching image
+            if os.path.exists(img_path):
+                
+                self.data[img_path] = {}    # Create new entry
+                results = task['annotations'][0]['result']  # All annotation info
 
-                    self.data[img_path] = {}
-                    results = task['annotations'][0]['result']
+                # Copy width and height from first annotation
+                self.data[img_path]['width'] = results[0]['original_width']
+                self.data[img_path]['height'] = results[0]['original_height']
 
-                    # Copy width and height from first annotation
-                    self.data[img_path]['width'] = results[0]['original_width']
-                    self.data[img_path]['height'] = results[0]['original_height']
-
-                    # All rle masks for the img
-                    self.data[img_path]['rles'] = [result['value']['rle'] for result in results]
+                # All rle masks for the img
+                self.data[img_path]['rle'] = [result['value']['rle'] for result in results]
 
     # Get all data
     def get(self):
+        """
+        Get the dict containing dataset information.
+
+        Returns: dict
+        """
         return self.data
     
-    # Display all items in the dataset along with the number of annotations
     def __str__(self):
         output = ''
         for imgPath in self.data:
             output += 'Image: ' + str(imgPath) + '\n'
             output += 'Original width: ' + repr(self.data[imgPath]['width']) + '\n'
             output += 'Original height: ' + repr(self.data[imgPath]['height']) + '\n'
-            output += 'Number of masks: ' + repr(len(self.data[imgPath]['rles'])) + '\n'
+            output += 'Number of masks: ' + repr(len(self.data[imgPath]['rle'])) + '\n'
 
         return output
 
+# Test Code
 dataset = Dataset('./dataset')
-
-print(dataset)
-
 data = dataset.get()
 
 for key in data:
@@ -68,7 +79,9 @@ for key in data:
     print('Reading labels for', img_path)
     width = data[key]['width']
     height = data[key]['height']
-    for rle in data[key]['rles']:
+    for rle in data[key]['rle']:
         mask = lc.rle_to_mask(rle, width, height)
         cv.imshow('Result', mask)
         cv.waitKey(0)
+
+cv.destroyAllWindows()
