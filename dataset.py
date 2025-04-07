04e7904ef455e6eff5ce96362cbac82b73c49d63
gavinsh32 <gavinhaynes32@gmail.com>
2025-04-07 15:48:06 -0700

    Fixed artifact issue: mask converted using labelconverter contained non-maximum values. Normalize range to [0.1] fixes issue.

--- dataset.py
+++ dataset.py
@@ -53,57 +53,28 @@ class Dataset:
                 
                 # List of masks corresponding to an image
                 results = task['annotations'][0]['result']
+                width = results[0]['original_width']
+                height = results[0]['original_height']
+                coco_rles = []
+
+                # Convert Label Studio RLE to COCO RLE
+                for result in results:
+                    rle = result['value']['rle']
+                    arr = lc.rle_to_mask(rle, width, height)
+                    arr[arr > 0] = 1
+                    arr = np.asfortranarray(arr, dtype=np.uint8)
+                    coco_rles.append(mask.encode(arr))
 
                 # Create a new entry with essential information   
-                self.insert(
-                    img_path,
-                    results[0]['original_width'],
-                    results[0]['original_height'],
-                    [result['value']['rle'] for result in results]
-                )
-
-    # Augment the dataset, scaling by constant factor scale
-    def augment(self, scale=5):
-        """
-        Augment images loaded in to the datset, scaling the volume by a constant factor. Augmented images are appended to current dataset and saved in the output folder.
-        Args:
-            scale:
-            Scale the total number of images by this amount
-        Returns: None
-        """
-
-        # Transformation Pipeline
-        transform = A.Compose([
-            A.HorizontalFlip(p=0.5),
-            A.RandomBrightnessContrast(p=0.2),
-        ])
-
-        for img_path in self.get():
-
-            input_masks = self.get_masks(img_path)
-            input_img = cv.imread(img_path, cv.COLOR_RGB2BGR)
-
-            for _ in range(scale):
-
-                result = transform(image=input_img, masks=input_masks)
-                
-                output_img = result['image']
-                output_masks = result['masks']
-                
-                ex = mask.encode(np.asfortranarray(output_masks[0]))
-                
-                print(ex)
+                self.insert(img_path, width, height, coco_rles)
 
     # Insert an image with annotations to internal dataset
-    def insert(self, key: str, width: int, height: int, rle: list[list[int]]):
-        self.data[key] = {'width': width, 'height': height, 'rle': rle}
+    def insert(self, key: str, width: int, height: int, rle: list[list]):
+        self.data[key] = {'width': width, 'height': height, 'rles': rle}
 
     # Get all data
     def get(self) -> dict:
         return self.data
-
-    def get_masks_rle(self, image_path: str) -> list[list]:
-        return self.get_entry(image_path)['rle']
 
     # Get the dict of information corresponding to img_path
     def get_entry(self, image_path: str) -> dict:
@@ -113,41 +84,26 @@ class Dataset:
             print('Error: dataset does not contain image', image_path)
             return {}
         
-    def get_mask_rle(self, index: str, mask_num: int) -> int:
-        if mask_num < self.num_masks(index):
-            return self.get_entry(index)['rle'][mask_num]
-        else:
-            print('Error: mask index', mask_num, 'out of range.')
-            return -1
+    # Get all RLE masks for a given image
+    def get_rles(self, img_path: str) -> list[list]:
+        return [rle for rle in self.get_entry(img_path)['rles']]
     
-    # Get all the numpy masks for an image
-    def get_masks(self, img_path: str) -> list[np.array]:
-        return [self.get_mask(img_path, i) for i in self.num_masks('img_path')]
-    
-    def get_mask(self, img_path: str, mask_num: int) -> np.array:
-        entry = self.get_entry(img_path)
-        width = entry['width']
-        height = entry['height']
-        rle = self.get_mask_rle(img_path, mask_num)
-        return lc.rle_to_mask(rle, height, width)
+    # Get all numpy masks for a given image
+    def get_masks_np(self, img_path: str) -> list[np.array]:
+        rles = self.get_rles(img_path)
+        return [mask.decode(rle) for rle in rles]
 
+    # Get available images in the dataset
     def indexes(self) -> list[str]:
         return [img_name for img_name in self.data]
 
+    # Return the number of images in the dataset
     def size(self) -> int:
         return len(self.data)
     
+    # Return the number of masks for a given image
     def num_masks(self, index: str) -> int:
-        return len(self.get_entry(index)['rle'])
-
-    def display(self, option: int) -> None:
-        pass
-    
-    def display_all(self, image_path: str):
-        pass
-
-    def display(self, image_path: str):
-        pass
+        return len(self.get_entry(index)['rles'])
 
     def display_entry(self, img_path: str) -> None:
         print('Image:', img_path)
@@ -162,28 +118,16 @@ class Dataset:
             output += 'Image: ' + str(imgPath) + '\n'
             output += 'Original width: ' + repr(self.data[imgPath]['width']) + '\n'
             output += 'Original height: ' + repr(self.data[imgPath]['height']) + '\n'
-            output += 'Number of masks: ' + repr(len(self.data[imgPath]['rle'])) + '\n'
+            output += 'Number of masks: ' + repr(len(self.data[imgPath]['rles'])) + '\n'
 
         print(output)
 
 dataset = Dataset('./dataset')
 
-mask_orig = dataset.get_mask('/home/gav/GitHub/TeamRAT/dataset/imgs/2.jpg', 0)
+masks = dataset.get_masks_np('/home/gav/GitHub/TeamRAT/dataset/imgs/3.jpg')
 
-# cv.imshow('Original Mask', mask_orig)
-# cv.waitKey(0)
+for i in masks:
+    cv.imshow('mask', i * 255)
+    cv.waitKey(0)
 
-mask_encoded = mask.encode(np.asfortranarray(mask_orig))
-#print(mask_encoded)
-
-mask_decoded = mask.decode(mask_encoded)
-
-mask_decoded = mask_decoded.reshape(mask_decoded.shape[0], mask_decoded.shape[1], 1)
-
-print(mask_decoded.dtype)
-print(mask_decoded.shape)
-print(np.unique(mask_decoded))
-
-cv.imshow('Mask', mask_decoded * 255)
-cv.waitKey(0)
 cv.destroyAllWindows()
