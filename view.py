50ef7bc4fe18e6e0f3db22f4a2f802bacb04d359
gavinsh32 <gavinhaynes32@gmail.com>
2025-06-18 16:13:16 -0700

    added some comments to view.py

--- view.py
+++ view.py
@@ -10,18 +10,22 @@ from pathlib import Path
 def main():
     assert len(sys.argv) == 3, 'Usage: python view.py <imgs_folder_path> <masks_folder_path>'
 
+    # Resolve the paths to the images and masks directories.
     imgs_dir: Path = Path(sys.argv[1]).resolve()
     masks_dir: Path = Path(sys.argv[2]).resolve()
 
+    # Fetch all masks for a given image.
     for img_path in imgs_dir.glob('*.jpg'):
         img: np.ndarray = cv.imread(str(img_path))
         collected_mask: np.ndarray = np.zeros_like(img)
 
+        # Load numpy mask, and OR to collect all masks for the image.
         for mask_path in masks_dir.glob(f'{img_path.stem}-*.npy'):
             mask: np.ndarray = np.load(mask_path)
             mask: np.ndarray = cv.cvtColor(mask, cv.COLOR_GRAY2BGR)
             collected_mask ^= mask
 
+        # Convert collected mask to a binary image for display.
         collected_mask[collected_mask > 0] = 255
         output: np.ndarray = cv.addWeighted(img, 1.0, collected_mask, 0.5, 0)
         
